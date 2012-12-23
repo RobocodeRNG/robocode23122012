36da429432e3ff239cea380746ccec54abb17c57
mabog <fish@suio.(none)>
2012-12-23 15:21:51 +0200

    More Damage

--- mabog/src/rgod.java
+++ mabog/src/rgod.java
@@ -4,13 +4,13 @@
      public void run() {
          while (true) {
              ahead(100);
-             turnGunRight(359);
+             turnGunRight(360);
              back(100);
              turnGunRight(360);
          }
      }
      public void onScannedRobot(ScannedRobotEvent e) {
-         fire(1);
+         fire(10);
      }
  }
   
